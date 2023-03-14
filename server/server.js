5a7865a1d96d2a50956fcdb0f1b7069884e79427
Prsasann Sampath Chandrasiri <89958555+ISampathI@users.noreply.github.com>
2023-03-14 18:44:09 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,22 +1,43 @@
-const express = require('express');
-const http = require('http');
-const socketio = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-let text = ''; // the text that needs to be typed by the players
-let players = []; // an array to store the current status of each player's typing progress
+let text = ""; // the text that needs to be typed by the players
+const MAX_PLAYERS_PER_ROOM = 2; // maximum number of players per room
+const rooms = []; // list of all active rooms
 
 // generate a random text for the game
 const generateText = () => {
-  const words = ['apple', 'banana', 'cherry', 'durian', 'elderberry', 'fig', 'grape', 'honeydew', 'kiwi', 'lemon', 'mango', 'nectarine', 'orange', 'pear', 'quince', 'raspberry', 'strawberry', 'tangerine', 'watermelon'];
+  const words = [
+    "apple",
+    "banana",
+    "cherry",
+    "durian",
+    "elderberry",
+    "fig",
+    "grape",
+    "honeydew",
+    "kiwi",
+    "lemon",
+    "mango",
+    "nectarine",
+    "orange",
+    "pear",
+    "quince",
+    "raspberry",
+    "strawberry",
+    "tangerine",
+    "watermelon",
+  ];
   const textLength = Math.floor(Math.random() * 6) + 15; // generate a random number between 15-20
-  let text = '';
+  let text = "";
   for (let i = 0; i < textLength; i++) {
     const randomIndex = Math.floor(Math.random() * words.length);
-    text += words[randomIndex] + ' ';
+    text += words[randomIndex] + " ";
   }
   return text.trim();
 };
@@ -24,7 +45,7 @@ const generateText = () => {
 const calculateProgress = (text, progress) => {
   const correctChars = text
     .slice(0, progress.length)
-    .split('')
+    .split("")
     .filter((char, i) => char === progress[i]).length;
 
   const percentage = (correctChars / text.length) * 100;
@@ -32,60 +53,92 @@ const calculateProgress = (text, progress) => {
 };
 
 // update the player's progress and emit the updated list of players to all clients
+// update the player's progress and emit the updated list of players to all clients
 const updateProgress = (socket, progress) => {
-  const playerIndex = players.findIndex((player) => player.id === socket.id);
-  if (playerIndex !== -1) {
-    players[playerIndex].progress = calculateProgress(text, progress);
-    io.emit('players', players);
+  const room = rooms.find(
+    (r) => r.players && r.players.some((p) => p.id === socket.id)
+  );
+  if (room) {
+    const player = room.players.find((p) => p.id === socket.id);
+    if (player) {
+      player.progress = calculateProgress(room.text, progress);
+      io.to(`room-${room.id}`).emit("players", room.players);
+    }
   }
 };
 
+const findAvailableRoom = () => {
+  let roomId;
+  for (const room of rooms) {
+    if (room.players.length < MAX_PLAYERS_PER_ROOM) {
+      roomId = room.id;
+      break;
+    }
+  }
+  if (!roomId) {
+    roomId = rooms.length + 1;
+    rooms.push({ id: roomId, players: [], text: "" });
+  }
+  return roomId;
+};
 
-// const updateProgress = (socket, progress) => {
-//   const playerIndex = players.findIndex((player) => player.id === socket.id);
-//   if (playerIndex !== -1) {
-//     const player = players[playerIndex];
-//     if (progress === text.slice(0, player.progress.length + 1)) {
-//       // Only update progress if the typed text matches the expected text
-//       players[playerIndex].progress = progress;
-//       io.emit('players', players);
-//     }
-//   }
-// };
-
-
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   console.log(`A new client has connected with id ${socket.id}`);
 
-  // add the new player to the list of players and emit the updated list to all clients
-  players.push({ id: socket.id, name: `Player ${players.length + 1}`, progress: '' });
-  io.emit('players', players);
+  const roomId = findAvailableRoom();
+
+  if (!rooms[roomId]) {
+    rooms[roomId] = { players: [], text: "" };
+  }
+
+  rooms[roomId].players.push({
+    id: socket.id,
+    name: `Player ${rooms[roomId].players.length + 1}`,
+    progress: "",
+  });
+  socket.join(`room-${roomId}`);
+  socket.emit("joinedRoom", roomId);
 
   // send the current text to the new player
-  socket.emit('text', text);
+  socket.emit("text", rooms[roomId].text);
 
   // handle the update progress event
-  socket.on('updateProgress', (progress) => {
-    updateProgress(socket, progress);
+  socket.on("updateProgress", (progress) => {
+    updateProgress(socket, progress, roomId);
   });
 
   // handle the start game event
-  socket.on('startGame', () => {
-    text = generateText();
-    players = players.map((player) => ({ ...player, progress: '' }));
-    io.emit('text', text);
-    io.emit('players', players);
+  socket.on("startGame", () => {
+    rooms[roomId].id = roomId;
+    rooms[roomId].text = generateText();
+    rooms[roomId].players = rooms[roomId].players.map((player) => ({
+      ...player,
+      progress: "",
+    }));
+    console.log("##", roomId);
+    io.to(`room-${roomId}`).emit("text", rooms[roomId].text);
+    io.to(`room-${roomId}`).emit("players", rooms[roomId].players);
   });
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     console.log(`The client with id ${socket.id} has disconnected`);
 
-    // remove the player from the list of players and emit the updated list to all clients
-    players = players.filter((player) => player.id !== socket.id);
-    io.emit('players', players);
+    // remove the player from the room and emit the updated list to all clients in the room
+    const playerIndex = rooms[roomId].players.findIndex(
+      (player) => player.id === socket.id
+    );
+    if (playerIndex !== -1) {
+      rooms[roomId].players.splice(playerIndex, 1);
+      io.to(`room-${roomId}`).emit("players", rooms[roomId].players);
+    }
+
+    // if the room is now empty, delete it
+    if (rooms[roomId].players.length === 0) {
+      delete rooms[roomId];
+    }
   });
 });
 
 server.listen(5000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 5000");
 });
