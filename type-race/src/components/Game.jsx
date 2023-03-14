08880391ee717c8305c37b0d8521dec40e0c6ea4
Prsasann Sampath Chandrasiri <89958555+ISampathI@users.noreply.github.com>
2023-03-14 20:58:45 +0530

    car race

--- type-race/src/components/Game.jsx
+++ type-race/src/components/Game.jsx
@@ -1,46 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import io from 'socket.io-client';
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import "./game.scss";
 
-const socket = io('http://localhost:5000', {
+const socket = io("http://localhost:5000", {
   transports: ["websocket", "polling", "flashsocket"],
 });
 
 const TyperaceMultiplayer = () => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [players, setPlayers] = useState([]);
 
   useEffect(() => {
-    socket.on('players', (players) => {
+    socket.on("players", (players) => {
       setPlayers(players);
       console.log(players);
     });
 
-    socket.on('text', (text) => {
+    socket.on("text", (text) => {
       setText(text);
     });
   }, []);
 
   const handleInputChange = (event) => {
     // update the current player's typing progress and send it to the server via socket.io
-    socket.emit('updateProgress', event.target.value);
+    socket.emit("updateProgress", event.target.value);
   };
 
   const handleStartGame = () => {
     // start the game and send a request to the server to generate a new text
-    socket.emit('startGame');
+    socket.emit("startGame");
   };
 
   return (
-    <div>
+    <div className="Game">
       <h1>Typerace Multiplayer Game</h1>
       <p>{text}</p>
       <input type="text" onChange={handleInputChange} />
       <button onClick={handleStartGame}>Start Game</button>
       <h2>Scoreboard</h2>
-      <ul>
+      {/* <ul>
         {players.map((player) => (
           <li key={player.id}>
             {player.name}: {player.progress}
+          </li>
+        ))}
+      </ul> */}
+      <ul className="car-tracks">
+        {players.map((player) => (
+          <li key={player.id}>
+            {player.name}: {player.progress}
+            <div className="car-track">
+              {" "}
+              <div className="car" style={{ marginLeft: player.progress }}>
+                🚗
+              </div>
+            </div>
           </li>
         ))}
       </ul>
